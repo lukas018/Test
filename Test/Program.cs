21b6e245d24ed9cc6e608a8c26437c3d2099a83b
lukas018 <lukas018.lundmark@gmail.com>
2015-01-29 14:11:41 +0100

    some more changes

--- Test/Program.cs
+++ Test/Program.cs
@@ -17,6 +17,9 @@ namespace Test
             int i = 0;
             int y = 2;
 
+
+
+            int a = 1;
             int z = 1;
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
